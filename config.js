888c157bd640bc849b8676e405897063631f71d0
Ravana-LK <loadilisha@gmail.com>
2026-01-09 07:50:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,18 +6,16 @@ function convertToBool(text, fault = 'true') {
 
 //gg
 module.exports = {
-SESSION_ID: 'VISPER-MD&e76d25d6-bbff-4f1f-9806-a1a7e9aeb50f',
+SESSION_ID: 'put your session id here', // RAVANA-UID&
 ANTI_DELETE: process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE, 
 MV_BLOCK: process.env. MV_BLOCK === undefined ? 'true' : process.env. MV_BLOCK,    
 ANTI_LINK: process.env.ANTI_LINK === undefined ? 'true' : process.env.ANTI_LINK, 
 SEEDR_MAIL: '',
 SEEDR_PASSWORD: '',
 SUDO: '',//
-DB_NAME: 'pramaaaataaaaaaaaaaaajjaa',
+DB_NAME: 'RavanaDataBase',
 LANG: 'SI',
-OWNER_NUMBER: '94724884317',
-TG_GROUP: 'https://t.me/+Zm865mJ_TL0yNGVl'
+OWNER_NUMBER: '94754871798',
+TG_GROUP: 'https://t.me/+SrilankaRavanaBotz'
 
 };
-//GITHUB_AUTH_TdOKEN: 'ouvnI0xSDsmfWA1filVxx.SZ0vJGYkjlC5VX54U0e10',
-
